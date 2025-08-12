5528d52875ba2f1c5904845e0f3d82b884550ac0
Aayush Joshi <aayushjoshi1006@gmail.com>
2025-08-12 20:56:05 +0530

    resolved minor production error

--- src/utlis/cartAPI.js
+++ src/utlis/cartAPI.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://e-commerce-vkhx.onrender.com";
 
 export const addToCartAPI = async (productId) => {
   const res = await fetch(`${BASE_URL}/cart`, {
